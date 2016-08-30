504d37a43622171590b6b0a91a16063d3646f72c
Joanna Tzu-Hsuan Huang <joannathhuang@Joannas-MacBook-Pro.local>
2016-08-30 16:01:14 -0400

    Done

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,25 +1,19 @@
-func printAge(name: String) {
+func printAge(name: String){
     print("\(name) is 29")
 }
 
-printAge("Adam")
+printAge("John")
 
 
-func happyBirthday(age: Int) {
+func happyBirthday(age: Int){
     print("Happy birthday #\(age)!")
 }
 
+happyBirthday(27)
 
 func getAge(name: String) -> Int {
     return 29
 }
-
-// Uncomment to see the error
-/*
-func badGetAge(name: String) -> Int {
-    print("\(name) is 29")
-}
-*/
 
 
 let friend = "Emily"
@@ -31,15 +25,22 @@ func birthdayGreeting(age: Int) -> String {
     return "Happy birthday #\(age)!"
 }
 
+birthdayGreeting(30)
+
+
 var greeting = birthdayGreeting(30)
 print(greeting)
 
 
-func getAgeAndCongratulate(name: String) -> Int {
+func getAgeAndCongratulate(name: String) -> Int{
     let age = 29
-    print("Happy \(age)th birthday, \(name)!")
+    print ("Happy \(age)th birthday, \(name)!")
     return age
 }
+getAgeAndCongratulate("David")
 
 let friend2 = "Pete"
 var friend2Age = getAgeAndCongratulate(friend2)
+
+let friend3 = "Jane"
+var friend3Age = getAgeAndCongratulate(friend3)
